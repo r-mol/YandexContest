446164e33cc079b3619ad78e7e510e59907b590f
Roman Molochkov <molochkov2017@gmail.com>
2022-05-03 09:57:38 +0300

    Add : solve of task 1-C

---
+++ 1/src/C.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class C {
+    public static void main(String[] args){
+        Scanner scanner  =  new Scanner(System.in);
+        int dm = scanner.nextInt();
+        int md = scanner.nextInt();
+        int year = scanner.nextInt();
+        System.out.println(checker(dm,md,year));
+    }
+
+    public static int checker (int dm,int md,int year) {
+        if(year >= 1970 && year <= 2069) {
+            if ((dm > 12 && md <= 12) ||(dm <= 12 && md > 12) || dm == md) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
