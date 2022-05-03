d7f1b6c291c62e55cb558670fb5b78242c8ff0b2
Roman Molochkov <molochkov2017@gmail.com>
2022-05-03 11:03:48 +0300

    Add : solve of task 1-D

---
+++ 1/src/D.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class D {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = (int) Math.floor(scanner.nextInt() / 2.0);
+        while (num != 0) {
+            scanner.nextInt();
+            num--;
+        }
+        System.out.println(scanner.nextInt());
+    }
+}
